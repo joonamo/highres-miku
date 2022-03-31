f8072575c36f69a719463b298278ae9a02cbf18f
joonamo <joonamo@users.noreply.github.com>
2022-03-31 20:05:34 +0300

    Tweak voting info

--- src/VotingInfo.tsx
+++ src/VotingInfo.tsx
@@ -5,12 +5,18 @@ export const VotingInfo = () => (
     <div className="card-content">
       <p className="subtitle">
         Voting for Snow Miku 2023 is open 31st March ~ 11th April 2022.{" "}
-        <a href="https://event.goodsmile.info/snowmiku2023/en/" target="blank">
-          Vote here!{" "}
-          <span className="icon">
-            <i className="fas fa-external-link-alt" />
-          </span>
-        </a>
+        <span>
+          <a
+            href="https://event.goodsmile.info/snowmiku2023/en/"
+            target="blank"
+          >
+            {/* eslint-disable-next-line no-irregular-whitespace */}
+            Vote here!{" "}
+            <span className="icon">
+              <i className="fas fa-external-link-alt" />
+            </span>
+          </a>
+        </span>
       </p>
     </div>
   </div>
